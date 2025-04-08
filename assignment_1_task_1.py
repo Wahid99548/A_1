ae8abaefb6da56a0453048cb06602223e934c658
Wahid99548 <143874634+Wahid99548@users.noreply.github.com>
2025-04-08 22:38:09 +0530

    Add files via upload

---
+++ assignment_1_task_1.py
@@ -0,0 +1,9 @@
+F_number = int(input("Enter the first number: "))
+S_number = int(input("Enter the second number: "))
+
+print("Addition: ",F_number+S_number)
+print("Subtraction: ",F_number-S_number)
+print("Multiplication: ",F_number*S_number)
+print("Division: ",F_number/S_number)
+ 
+
---
+++ assignment_1_task_2.py
@@ -0,0 +1,4 @@
+Fname= input("Enter your first name: ")
+Sname = input("Enter your last name: ")
+
+print("Hello, "+Fname+" "+Sname+"! Welcome to the Python program.")
